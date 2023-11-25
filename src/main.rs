49d256e1ac8f98982612dd0e46978e01cdf50c70
Chris Boyce <cboyce@sonic.net>
2023-11-25 09:48:26 -0800

    Adding egui

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,69 @@
 use nannou::prelude::*;
 use nannou_audio as audio;
 use nannou_audio::Buffer;
+use nannou_egui::{self, egui, Egui};
 use std::f64::consts::PI;
 
 fn main() {
-    nannou::app(model).run();
+    nannou::app(model).update(update).run();
 }
 
 struct Model {
+    egui: Egui,
+    settings: Settings,
     stream: audio::Stream<Audio>,
 }
 
+struct Settings {
+    resolution: u32,
+    scale: f32,
+    rotation: f32,
+    color: Srgb<u8>,
+    position: Vec2,
+}
 struct Audio {
     phase: f64,
     hz: f64,
 }
+fn update(_app: &App, model: &mut Model, update: Update) {
+    let egui = &mut model.egui;
+    let settings = &mut model.settings;
 
+    egui.set_elapsed_time(update.since_start);
+    let ctx = egui.begin_frame();
+
+    egui::Window::new("Settings").show(&ctx, |ui| {
+        // Resolution slider
+        ui.label("Resolution:");
+        ui.add(egui::Slider::new(&mut settings.resolution, 1..=40));
+
+        // Scale slider
+        ui.label("Scale:");
+        ui.add(egui::Slider::new(&mut settings.scale, 0.0..=1000.0));
+
+        // Rotation slider
+        ui.label("Rotation:");
+        ui.add(egui::Slider::new(&mut settings.rotation, 0.0..=360.0));
+
+        // Random color button
+        let clicked = ui.button("Random color").clicked();
+
+        if clicked {
+            settings.color = rgb(random(), random(), random());
+        }
+    });
+}
 fn model(app: &App) -> Model {
     // Create a window to receive key pressed events.
-    app.new_window()
+    let window_id = app
+        .new_window()
         .key_pressed(key_pressed)
         .view(view)
+        .raw_event(raw_window_event)
         .build()
         .unwrap();
+    let window = app.window(window_id).unwrap();
+    let egui = Egui::from_window(&window);
 
     // Initialise the audio API so we can spawn an audio stream.
     let audio_host = audio::Host::new();
@@ -41,7 +82,17 @@ fn model(app: &App) -> Model {
 
     stream.play().unwrap();
 
-    Model { stream }
+    Model {
+        egui,
+        stream,
+        settings: Settings {
+            resolution: 10,
+            scale: 200.0,
+            rotation: 0.0,
+            color: WHITE,
+            position: vec2(0.0, 0.0),
+        },
+    }
 }
 
 // A function that renders the given `Audio` to the given `Buffer`.
@@ -91,6 +142,24 @@ fn key_pressed(_app: &App, model: &mut Model, key: Key) {
     }
 }
 
-fn view(_app: &App, _model: &Model, frame: Frame) {
-    frame.clear(DIMGRAY);
+fn raw_window_event(_app: &App, model: &mut Model, event: &nannou::winit::event::WindowEvent) {
+    // Let egui handle things like keyboard and mouse input.
+    model.egui.handle_raw_event(event);
+}
+fn view(app: &App, model: &Model, frame: Frame) {
+    let settings = &model.settings;
+
+    let draw = app.draw();
+    draw.background().color(BLACK);
+
+    let rotation_radians = deg_to_rad(settings.rotation);
+    draw.ellipse()
+        .resolution(settings.resolution as f32)
+        .xy(settings.position)
+        .color(settings.color)
+        .rotate(-rotation_radians)
+        .radius(settings.scale);
+
+    draw.to_frame(app, &frame).unwrap();
+    model.egui.draw_to_frame(&frame).unwrap();
 }
